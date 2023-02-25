35791311bfbbf625ef0341f738cca0df3c657dc7
Ruben Garcia <rgarcr09@gmail.com>
2023-02-25 16:38:50 -0500

    Compare Cards
    Selected two cards, compare image source and classes.
    Compare classes to avoid selected the same exact card
    Compare Images to find a matching pair and keep the card Images
    Else change images to original.

--- main.js
+++ main.js
@@ -19,21 +19,21 @@ object{
 */
 
 let images = document.querySelectorAll("div>img");
-for(let elem of images){
+for (let elem of images) {
     elem.addEventListener("click", flipCard);
 }
 
 //create array of random numbers
-function createRandomList(){
+function createRandomList() {
     let arr = [];
     let num = Math.floor(Math.random() * (13 - 1) + 1);
     arr.push(num);
-     while(arr.length != 12){
-         num = Math.floor(Math.random() * (13 - 1) + 1);
-         if(!arr.includes(num))
-           arr.push(num)
+    while (arr.length != 12) {
+        num = Math.floor(Math.random() * (13 - 1) + 1);
+        if (!arr.includes(num))
+            arr.push(num)
         //console.log(num, arr)   
-     }
+    }
     return arr;
 }
 
@@ -50,19 +50,53 @@ let cardObj = {
     pair6: [arr[10], arr[11], "./imgs/pancakes.jpg"]
 };
 
+let count = 0;
+//array to hold selected cards
+let cardsSelected = []
 //loop through cardsObject, check if any attributes have a matching number to
 //the end of the classList of the selected
-function flipCard(element){
+function flipCard(element) {
+    if (count < 2)
+        for (key in cardObj) {
+            //loop through keys loop
+            let num1 = cardObj[key][0];
+            let num2 = cardObj[key][1]
+            let img = cardObj[key][2]
+            //console.log(key, num1, num2)
+            if (element.target.classList.contains('card' + num1) || element.target.classList.contains('card' + num2)) {
+                count++;
+                cardsSelected.push(element.target)
+                element.target.src = img;
 
-    for (key in cardObj) {
-        //loop through keys loop
-        let num1 = cardObj[key][0];
-        let num2 = cardObj[key][1]
-        let img = cardObj[key][2]
-        //console.log(key, num1, num2)
-        if(element.target.classList.contains('card'+num1) || element.target.classList.contains('card' + num2)){
-            element.target.src=img;    
-            return;
+                /*
+                Compare when two cards are selected
+                set cardSelected list to 0 
+                change images to ingredients if not the same
+                */
+                if (count === 2) {
+                    if(cardsSelected[0].src === cardsSelected[1].src && cardsSelected[0].classList != cardsSelected[1].classList){
+                        cardsSelected.length = 0;
+                        count=0;
+                    }
+                    else{
+                        //add delay timer 
+                        setTimeout(() => {
+                            count = 0;
+                            for (let elem of cardsSelected)
+                                elem.src = "./imgs/ingredients.jpg";
+                            console.log(cardsSelected);
+                            cardsSelected.length = 0;
+                          }, "1000")
+
+                    }
+                }
+            }
         }
-    }    
+
+
+}
+
+//check if two cards are up
+function checkIfUp() {
+
 }
